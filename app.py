d5961a3b903aa5a58606110ec51c0279b48eca67
Ixtiyor Jurayev <ixtiyorjurayev05@gmail.com>
2026-05-11 19:18:35 +0500

    update project

--- app.py
+++ app.py
@@ -1421,18 +1421,18 @@ def certificate_pdf():
         return redirect('/login')
 
     # Positions (qo'lda sozlash uchun)
-    name_y = 640  # Nom uchun y pozitsiyasi
+    name_y = 500  # Nom uchun y pozitsiyasi
     name_x_offset = -300  # Nom uchun x offset (markazdan)
     name_y_offset = 0  # Nom uchun qo'shimcha vertikal siljitish
-    result_y = 850  # Natija uchun y pozitsiyasi
+    result_y = 700  # Natija uchun y pozitsiyasi
     result_x_offset = -600  # Natija uchun x offset (markazdan)
     result_y_offset = 0  # Natija uchun qo'shimcha vertikal siljitish
     test_x_offset = -700  # Test label uchun x offset (markazdan)
     test_y_offset = 460  # Test label uchun y offset (result_y ga nisbatan)
     qr_x_offset = 400  # QR kodning o'ngdan masofasi
     qr_y_offset = 330  # QR kodning pastdan masofasi
-    font_size_name = 80  # Nom uchun shrift o'lchami
-    font_size_result = 30  # Natija uchun shrift o'lchami
+    font_size_name = 150  # Nom uchun shrift o'lchami
+    font_size_result = 60  # Natija uchun shrift o'lchami
     qr_size = 170  # QR kod o'lchami
 
     conn = sqlite3.connect("users.db")
@@ -1528,7 +1528,7 @@ def certificate_pdf():
         if font_path:
             font_name = ImageFont.truetype(font_path, font_size_name)
             font_result = ImageFont.truetype(font_path, font_size_result)
-            font_test = ImageFont.truetype(font_path, 40)
+            font_test = ImageFont.truetype(font_path, 60)
         else:
             font_name = ImageFont.load_default()
             font_result = ImageFont.load_default()
